aae467680e1637b5c574946dc6af6691755009f4
alimulquran <alimulquran@hotmail.com>
2023-11-26 16:42:35 +0400

    Portal - Alim ul Quran

---
+++ portal/application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'BCJJ2H-UBLEFH-TLZXSB-R202MWFpSDJHOHJWeXV1OFdHS1gyWDJUdGRvcE9nMklHb0srekRjQ2pnWT0=';
+$config['app_ver'] = 1;
